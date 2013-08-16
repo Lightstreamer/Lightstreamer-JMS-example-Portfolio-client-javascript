dc5871c3af4741f81fda804e4cc3dd11c3207db7
Weswit <giuseppe.corti@lightstreamer.com>
2013-08-16 12:59:15 +0200

    First commit

---
+++ README.md
@@ -0,0 +1,57 @@
+# Lightstreamer JMS Gateway Portfolio Demo Client for JavaScript #
+
+This project includes a front-end example based on the Basic Portfolio Demo for Lightstreamer JMS gateway.
+
+## JMS Portfolio Demo ##
+
+In the JMS Portfolio Demo, a virtual stock portfolio, shared among all the connected users, is displayed.
+By using the "Submit Order" panel, you can buy or sell a stock (identified by an item number), filling in the Quantity field and pressing the proper button.<br>
+The portfolio grid is updated in push mode, for both the columns and the rows (this is the so called "metapush" feature).<br>
+This page uses the <b>JMS JS Client API for Lightstreamer</b> on top of <b>JavaScript Client API for Lightstreamer</b> to handle the communications with Lightstreamer Server.<br>
+
+Check out the sources for further explanations. 
+
+# Deploy #
+
+Before you can run the demo of this project some dependencies need to be solved:
+
+-  Get the lightstreamer.js file from the [latest Lightstreamer distribution](http://www.lightstreamer.com/download) and put it in the root folder of this project.
+-  Get the require.js file form [requirejs.org](http://requirejs.org/docs/download.html) and put it in the root folder of this project.
+-  Get the lightstreamer-jms.js file from the [Lightstreamer JMS Gateway preview](http://www.lightstreamer.com/download) (please click the "From the Labs" section) and put it in the root folder of this project.
+
+Now, you need to configure the index.html of this example by specifying the name of the data adapter you are going to use. By default the demo will look for the <b>HornetQ</b> data adapter, please refer to the related [Service project]() for more details on the choice of a JMS broker to be used. 
+To set the data adapter name and the connection name look where the connection is created:
+
+```js
+  ConnectionFactory.createConnection(function(conn) {
+    ...
+    ...
+    ...
+  }, "http://localhost:8080/", "JMS", "HornetQ", // Change data adapter here
+    ...
+    ...
+    ...
+  });
+```
+
+To access the demo from a web browser copy it somewhere under you root webserver directory. You can also add it to the standard Lightstreamer demo pages under "LightstreamerHome/pages/demos" directory and access it as: <i>http://<your lightstreamer http address>/demos/StockListDemo_JMS/</i>.
+
+You can also simply start by opening the index.html file with your browser.
+
+
+# See Also #
+
+## Lightstreamer back-end Service needed by these demo clients ##
+
+* To be add: [Lightstreamer JMS Gateway Portfolio Demo Service]()
+
+## Similar demo clients that may interest you ##
+
+* [Lightstreamer JMS Gateway StockList Demo Client for JavaScript](https://github.com/Weswit/Lightstreamer-JMS-example-StockList-client-javascript)
+* [Lightstreamer JMS Gateway Chat Demo Client for JavaScript](https://github.com/Weswit/Lightstreamer-JMS-example-Chat-client-javascript)
+* [Lightstreamer StockList Demo Client for javascript](https://github.com/Weswit/Lightstreamer-example-StockList-client-javascript)
+
+# Lightstreamer Compatibility Notes #
+
+- Compatible with Lightstreamer JavaScript Client library version 6.0 or newer.
+- Compatible with Lightstreamer JMS Gateway Adapter since version 1.0 or newer.
---
+++ portfolio.js
@@ -0,0 +1,163 @@
+/*
+Copyright 2013 Weswit Srl
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+// Lightstreamer JMS Portfolio Demo
+// Table Management
+
+//////////////// Portfolio Table Management
+
+// The current portfolio should be chosen by the user according to the user profile;
+// in this sample, user authentication is not included and a single portfolio is
+// shared among all the connected users
+var portfolioId= "portfolio1";
+
+// portfolio contents; provided by the PORTFOLIO_ADAPTER in COMMAND mode
+var fieldList= ["key", "command", "qty"];
+
+// cell highlighting time (milliseconds)
+var hotTime= 500;
+
+// will contain a reference to the DynaGrid instance
+var portfolioGrid= null;
+
+//////////////// Grid Sort Management
+
+var initialSort= "key";
+var direction= false; // True = decreasing; false = increasing; null = no sort
+
+function changeSort(sortOn) {
+	var sortedBy = portfolioGrid.getSortField();
+
+	if (sortOn == sortedBy) {
+		if (direction == false) {
+			direction = true;
+			document.getElementById("img_" + sortOn).src = "images/down.gif";
+
+		} else if (direction == true) {
+			direction = null;
+			document.getElementById("img_" + sortOn).src = "images/spacer.gif";
+			document.getElementById("col_" + sortOn).className = "tableTitle";
+
+		} else {
+			direction = false;
+			document.getElementById("img_" + sortOn).src = "images/up.gif";
+		}
+
+	} else {
+		direction = false;
+
+		if (sortedBy != null) {
+			document.getElementById("img_" + sortedBy).src = "images/spacer.gif";
+			document.getElementById("col_" + sortedBy).className = "tableTitle";
+		}
+
+		document.getElementById("img_" + sortOn).src = "images/up.gif";
+		document.getElementById("col_" + sortOn).className = "tableTitleSorted";
+	}
+
+	if (direction == null) {
+		portfolioGrid.setSort(null);
+
+	} else {
+		if (sortOn == "qty")
+			portfolioGrid.setSort(sortOn, direction, true, false);
+		else
+			portfolioGrid.setSort(sortOn, direction);
+	}
+}
+
+//////////////// Connection and Grid setup
+
+var topicSession= null;
+var queueSession= null;
+
+require(["ConnectionFactory", "DynaGrid", "StatusWidget"], function(ConnectionFactory, DynaGrid, StatusWidget) {
+	ConnectionFactory.createConnection(function(conn) {
+
+		// Connection succeeded, topic subscription
+		topicSession= conn.createSession(false, "PRE_ACK");
+		var topic= topicSession.createTopic("portfolioTopic");
+		var consumer= topicSession.createConsumer(topic, null);
+
+		portfolioGrid= new DynaGrid("portfolio",true);
+		portfolioGrid.setAutoCleanBehavior(true,false);
+		portfolioGrid.addListener({
+			onVisualUpdate: function(key,info) {
+				if (info == null) {
+
+					// Cleaning
+					return;
+				}
+
+				// visual effects on updates
+				info.setHotTime(hotTime);
+				info.setStyle("lshot", "lscold");
+				info.setCellStyle("command", "commandhot", "commandcold")
+			}
+		});
+
+		// Let's define the initial sorting column
+		changeSort(initialSort);
+
+		// Add listener to message consumer
+		consumer.setMessageListener({
+			onMessage: function(message) {
+
+				// Message received
+				var portfolioMessage= message.body;
+
+				// Update the dyna grid
+				portfolioGrid.updateRow(portfolioMessage.key, portfolioMessage);
+			}
+		});
+
+		// Start the connection
+		conn.start();
+
+		// Send subscription message for portfolio
+		queueSession= conn.createSession(false, "AUTO_ACK");
+		var queue= queueSession.createQueue("portfolioQueue");
+		var producer= queueSession.createProducer(queue, null);
+
+		var msg= queueSession.createTextMessage("SUBSCRIBE|" + portfolioId);
+		producer.send(msg);
+
+	}, function(errorCode, errorMessage) {
+
+		// Connection failed, show the error
+		alert("Error: " + errorCode + " " + errorMessage);
+
+	}, "http://localhost:8080/", "JMS", "HornetQ",  // Change data adapter here
+		null, null, function(lsClient) {
+
+		// Enable connection sharing and status widget (optional)
+		lsClient.connectionSharing.enableSharing("JMSDemoCommonConnection", "ATTACH", "CREATE");
+		lsClient.addListener(new StatusWidget("left", "0px", true));
+
+		// Add listener to enable buttons only when connected
+		lsClient.addListener({
+			onStatusChange: function(newStatus) {
+				if (newStatus.indexOf("CONNECTED") == 0) {
+					document.getElementById("buy").disabled= false;
+					document.getElementById("sell").disabled= false;
+
+				} else {
+					document.getElementById("buy").disabled= true;
+					document.getElementById("sell").disabled= true;
+				}
+			}
+		});
+	});
+});
