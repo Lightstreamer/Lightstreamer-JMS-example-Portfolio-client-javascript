7218ecd7f7064c088b2079426ee247c78a599f53
mone <simone.fabiano@gmail.com>
2014-09-10 17:36:43 +0200

    isolate grid code in a separate file to slim down portfolio.js

---
+++ grid.js
@@ -0,0 +1,82 @@
+define(["DynaGrid"],function(DynaGrid) {
+  
+  //cell highlighting time (milliseconds)
+  var hotTime= 500;
+  var direction= false; // True = decreasing; false = increasing; null = no sort
+
+  
+  var portfolioGrid= new DynaGrid("portfolio",true);
+  portfolioGrid.setAutoCleanBehavior(true,false);
+  portfolioGrid.addListener({
+    onVisualUpdate: function(key,info) {
+      if (info == null) {
+
+        // Cleaning
+        return;
+      }
+
+      // visual effects on updates
+      info.setHotTime(hotTime);
+      info.setStyle("lshot", "lscold");
+      info.setCellStyle("command", "commandhot", "commandcold");
+    }
+  });
+  
+  
+  var SPACER = null;
+  var UP = "images/up.gif";
+  var DOWN = "images/down.gif";
+  
+  var gridWrap = {
+    
+    updateRow: function(key,data) {
+      portfolioGrid.updateRow(key,data);
+    },
+    
+    changeSort: function(sortOn) {
+      var sortedBy = portfolioGrid.getSortField();
+      var arrow = null;
+      
+      if (sortOn != sortedBy || direction === null) {
+        direction = false;
+        arrow = UP;
+      } else if (direction === false) {
+        direction = true;
+        arrow = DOWN;
+      } else {
+        direction = null;
+      }
+      
+      if (sortOn != sortedBy || direction === null) {
+        var currentImg = $("#img_" + sortedBy);
+        var currentCol = $("#col_" + sortedBy);
+        
+        currentCol.removeClass("tableTitleSorted");
+        currentImg.attr("src",SPACER);
+      }
+      
+      if (direction !== null) {
+        var nextImg = $("#img_" + sortOn);
+        var nextCol = $("#col_" + sortOn);
+        
+        nextImg.attr("src",arrow);
+        nextCol.addClass("tableTitleSorted");
+        
+        if (sortOn == "qty") {
+          portfolioGrid.setSort(sortOn, direction, true, false);
+        } else {
+          portfolioGrid.setSort(sortOn, direction);
+        }
+      } else {
+        portfolioGrid.setSort(null);
+      }
+      
+    }
+  };
+  
+  $(".button[data-sorting]").click(function() {
+    gridWrap.changeSort($(this).data("sorting"));
+  });
+  
+  return gridWrap;
+});
--- portfolio.js
+++ portfolio.js
@@ -23,148 +23,77 @@ limitations under the License.
 // shared among all the connected users
 var portfolioId= "portfolio1";
 
-// portfolio contents; provided by the PORTFOLIO_ADAPTER in COMMAND mode
-var fieldList= ["key", "command", "qty"];
+//////////////// Connection setup
 
-// cell highlighting time (milliseconds)
-var hotTime= 500;
-
-// will contain a reference to the DynaGrid instance
-var portfolioGrid= null;
-
-//////////////// Grid Sort Management
-
-var initialSort= "key";
-var direction= false; // True = decreasing; false = increasing; null = no sort
-
-function changeSort(sortOn) {
-	var sortedBy = portfolioGrid.getSortField();
-
-	if (sortOn == sortedBy) {
-		if (direction == false) {
-			direction = true;
-			document.getElementById("img_" + sortOn).src = "images/down.gif";
-
-		} else if (direction == true) {
-			direction = null;
-			document.getElementById("img_" + sortOn).src = "images/spacer.gif";
-			document.getElementById("col_" + sortOn).className = "tableTitle";
-
-		} else {
-			direction = false;
-			document.getElementById("img_" + sortOn).src = "images/up.gif";
-		}
-
-	} else {
-		direction = false;
-
-		if (sortedBy != null) {
-			document.getElementById("img_" + sortedBy).src = "images/spacer.gif";
-			document.getElementById("col_" + sortedBy).className = "tableTitle";
-		}
-
-		document.getElementById("img_" + sortOn).src = "images/up.gif";
-		document.getElementById("col_" + sortOn).className = "tableTitleSorted";
-	}
-
-	if (direction == null) {
-		portfolioGrid.setSort(null);
-
-	} else {
-		if (sortOn == "qty")
-			portfolioGrid.setSort(sortOn, direction, true, false);
-		else
-			portfolioGrid.setSort(sortOn, direction);
-	}
-}
-
-//////////////// Connection and Grid setup
-
-var topicSession= null;
 var queueSession= null;
 
-require(["ConnectionFactory", "DynaGrid", "StatusWidget"], function(ConnectionFactory, DynaGrid, StatusWidget) {
-	ConnectionFactory.createConnection(function(conn) {
+require(["ConnectionFactory", "./grid", "StatusWidget"], function(ConnectionFactory, grid, StatusWidget) {
+  ConnectionFactory.createConnection(function(conn) {
 
-		// Connection succeeded, topic subscription
-		topicSession= conn.createSession(false, "PRE_ACK");
-		var topic= topicSession.createTopic("portfolioTopic");
-		var consumer= topicSession.createConsumer(topic, null);
+    // Connection succeeded, topic subscription
+    var topicSession= conn.createSession(false, "PRE_ACK");
+    var topic= topicSession.createTopic("portfolioTopic");
+    var consumer= topicSession.createConsumer(topic, null);
 
-		portfolioGrid= new DynaGrid("portfolio",true);
-		portfolioGrid.setAutoCleanBehavior(true,false);
-		portfolioGrid.addListener({
-			onVisualUpdate: function(key,info) {
-				if (info == null) {
+    
 
-					// Cleaning
-					return;
-				}
+    // Let's define the initial sorting column
+    grid.changeSort("key");
 
-				// visual effects on updates
-				info.setHotTime(hotTime);
-				info.setStyle("lshot", "lscold");
-				info.setCellStyle("command", "commandhot", "commandcold")
-			}
-		});
+    // Add listener to message consumer
+    consumer.setMessageListener({
+      onMessage: function(message) {
 
-		// Let's define the initial sorting column
-		changeSort(initialSort);
+        // Message received
+        var portfolioMessage= message.getObject();
 
-		// Add listener to message consumer
-		consumer.setMessageListener({
-			onMessage: function(message) {
+        // Update the dyna grid
+        grid.updateRow(portfolioMessage.key, portfolioMessage);
+      }
+    });
 
-				// Message received
-				var portfolioMessage= message.getObject();
+    // Start the connection
+    conn.start();
 
-				// Update the dyna grid
-				portfolioGrid.updateRow(portfolioMessage.key, portfolioMessage);
-			}
-		});
+    // Wait a moment before subscribing to avoid the subscribe
+    // message to get to the service before the topic subscription
+    // is actually started
+    setTimeout(function() {
 
-		// Start the connection
-		conn.start();
+      // Send subscription message for portfolio
+      queueSession= conn.createSession(false, "AUTO_ACK");
+      var queue= queueSession.createQueue("portfolioQueue");
+      var producer= queueSession.createProducer(queue, null);
 
-		// Wait a moment before subscribing to avoid the subscribe
-		// message to get to the service before the topic subscription
-		// is actually started
-		setTimeout(function() {
+      var msg= queueSession.createTextMessage("SUBSCRIBE|" + portfolioId);
+      producer.send(msg);
 
-			// Send subscription message for portfolio
-			queueSession= conn.createSession(false, "AUTO_ACK");
-			var queue= queueSession.createQueue("portfolioQueue");
-			var producer= queueSession.createProducer(queue, null);
+    }, 500);
 
-			var msg= queueSession.createTextMessage("SUBSCRIBE|" + portfolioId);
-			producer.send(msg);
+  }, function(errorCode, errorMessage) {
 
-		}, 500);
+    // Connection failed, show the error
+    alert("Error: " + errorCode + " " + errorMessage);
 
-	}, function(errorCode, errorMessage) {
+  }, "http://localhost:8080/", "JMS", "HornetQ",  // Change data adapter here
+    null, null, function(lsClient) {
 
-		// Connection failed, show the error
-		alert("Error: " + errorCode + " " + errorMessage);
+    // Enable connection sharing and status widget (optional)
+    lsClient.connectionSharing.enableSharing("JMSDemoCommonConnection", "ATTACH", "CREATE");
+    lsClient.addListener(new StatusWidget("left", "0px", true));
 
-	}, "http://localhost:8080/", "JMS", "HornetQ",  // Change data adapter here
-		null, null, function(lsClient) {
+    // Add listener to enable buttons only when connected
+    lsClient.addListener({
+      onStatusChange: function(newStatus) {
+        if (newStatus.indexOf("CONNECTED") == 0) {
+          document.getElementById("buy").disabled= false;
+          document.getElementById("sell").disabled= false;
 
-		// Enable connection sharing and status widget (optional)
-		lsClient.connectionSharing.enableSharing("JMSDemoCommonConnection", "ATTACH", "CREATE");
-		lsClient.addListener(new StatusWidget("left", "0px", true));
-
-		// Add listener to enable buttons only when connected
-		lsClient.addListener({
-			onStatusChange: function(newStatus) {
-				if (newStatus.indexOf("CONNECTED") == 0) {
-					document.getElementById("buy").disabled= false;
-					document.getElementById("sell").disabled= false;
-
-				} else {
-					document.getElementById("buy").disabled= true;
-					document.getElementById("sell").disabled= true;
-				}
-			}
-		});
-	});
+        } else {
+          document.getElementById("buy").disabled= true;
+          document.getElementById("sell").disabled= true;
+        }
+      }
+    });
+  });
 });
